bb33db57609410666e0e50fb0d55ea27c2353718
Manu Deepin <realgarcilaso@outlook.com>
2018-07-03 07:31:07 -0500

    02 creando relaciones
    avi 06. crea la srelaciones que hay entre esos modelos aunque algunas cosas no son similares al archivo original, en el archivoi\ origiinal en los modelos esta escrito como belogsto y en el video dice hasone, de seguro lo corregira. tambien puso el slug en post, cat, y tags.

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+
+    public function post()
+    {
+        return $this->hasOne(Post::class);
+        //return $this->belongsTo(Post::class);
+    }
+
+    public function author()
+    {
+        return $this->hasOne(User::class);
+        //return $this->belongsTo(User::class);
+    }
+
 }
