0bd49e007ad1670c423fd2800db8b9095731b503
Manu Deepin <realgarcilaso@outlook.com>
2018-07-04 10:21:53 -0500

    07 crud Etiquetas
    crea el crud de etiquetas. algo nuevo que vi es que si una etiqueta se duplica inmediatamente se le agrega un guiion y un numero.

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,6 @@ Route::group(['prefix'=>'admin','namespace'=>'Admin', ], function(){
 
     Route::get('/', 'DashboardController@index');
     Route::resource('/categories', 'CategoriesController');
-   
+    Route::resource('/tags', 'TagsController');
 
 });
