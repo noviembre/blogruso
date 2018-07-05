81f2fb1422dfa726d4fae75d8042e5af5b92c5f5
Manu Deepin <realgarcilaso@outlook.com>
2018-07-05 07:35:45 -0500

    09.5 adding routes in Sidebar

---
+++ resources/views/admin/_sidebar.blade.php
@@ -0,0 +1,13 @@
+<ul class="sidebar-menu">
+    <li class="header">MAIN NAVIGATION</li>
+    <li class="treeview">
+        <a href="#">
+            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+        </a>
+    </li>
+    <li><a href="{{route('posts.index')}}"><i class="fa fa-sticky-note-o"></i> <span>Posts</span></a></li>
+    <li><a href="{{route('categories.index')}}"><i class="fa fa-list-ul"></i> <span>Categories</span></a></li>
+    <li><a href="{{route('tags.index')}}"><i class="fa fa-tags"></i> <span>Tags</span></a></li>
+    <li><a href="{{route('users.index')}}"><i class="fa fa-users"></i> <span>Users</span></a></li>
+
+</ul>
