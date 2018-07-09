1aba2bdb040ff51765395d04ef591c7fbbec46ca
Manu Mint Laptop <realgarcilaso@outlook.com>
2018-07-09 10:31:04 -0500

    15 Ojo -Igmora este commit
    se crearon multiples archivoi llamados .directory asi que me force a ignorarlos de este modo ya que no podia con el gitignore aunque puse el codigo que ignora todos los archivos .directory de todos ,modos.

--- resources/views/pages/_sidebar.blade.php
+++ resources/views/pages/_sidebar.blade.php
@@ -3,7 +3,11 @@
 <div class="col-md-4" data-sticky_column>
     <div class="primary-sidebar">
 
-            @include('admin.errors')
+
+
+
+
+
 
         <aside class="widget">
             <h3 class="widget-title text-uppercase text-center">Popular Posts</h3>
